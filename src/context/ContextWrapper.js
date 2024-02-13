22f80056cfb94687eae94139855ac4d839af955a
samshadali <sam606166@gmail.com>
2024-02-13 22:45:53 +0530

    added localstorage to persist the data

--- src/context/ContextWrapper.js
+++ src/context/ContextWrapper.js
@@ -1,11 +1,16 @@
 import React, { createContext, useState } from 'react'
+import { useEffect } from 'react';
 
 export const context = createContext();
 function ContextWrapper({children}) {
-    const [html,sethtml] = useState('');
-    const [css,setcss] = useState('');
-    const [js,setjs] = useState('');
-
+    const [html,sethtml] = useState(localStorage.getItem('html') || '');
+    const [css,setcss] = useState(localStorage.getItem('css') || '');
+    const [js,setjs] = useState(localStorage.getItem('js') || '');
+    useEffect(()=>{
+      localStorage.setItem('html',html);
+      localStorage.setItem('css',css);
+      JSON.stringify(localStorage.setItem('js',js));
+    },[css,html,js])
   return (
     <context.Provider
     value={{
